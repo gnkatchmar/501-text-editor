a996f44d376cc1abec9d30070fbcb6cdf0fca7b5
gnkatchmar <gunk55@msn.com>
2017-08-19 17:32:32 -0700

    working checxbox

--- src/Input.js
+++ src/Input.js
@@ -3,29 +3,48 @@ import React, { Component } from 'react';
 class Input extends Component {
   constructor(props) {
     super(props);
-    this.state = { message: '' };
+    this.state = { text: '', bold: false };
     this.handleUpdate = this.handleUpdate.bind(this);
+    this.checkBold = this.checkBold.bind(this);
   }
 
   handleUpdate(event) {
-    this.setState({ message: event.target.value });
+    this.setState({ text: event.target.value });
   }
+
+  checkBold(event) {
+    this.setState({ bold: event.target.checked });
+  }
+
+
 
   render() {
     return (
       <div className="Editor">
-      Message: 
+      text: 
         <input
           type="text"
           onChange={this.handleUpdate}
-          value={this.state.message}
+          value={this.state.text}
         />
+        <p></p>
+        Bold?:
+        <input 
+          type="checkbox"
+          id="bold"
+          onChange={this.checkBold} 
+          checked={this.state.bold}
+        />
+
+
+
+
+
         <hr></hr>
-
-
         <p>Output:</p>
         <textarea rows="10" cols="50"
-          value={this.state.message}
+          value={this.state.text}
+          style={{fontWeight: this.state.bold ? "bold" : "normal"}}
         />
 
       </div>
